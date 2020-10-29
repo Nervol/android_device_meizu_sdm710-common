69119dc02df44e7751503d89d8732e49762a4223
blackrow67 <dogukanbey42@gmail.com>
2020-10-29 21:49:58 +0300

    Fingerprint....

--- common.mk
+++ common.mk
@@ -125,7 +125,8 @@ PRODUCT_PACKAGES += \
 
 # Fingerprint
 PRODUCT_PACKAGES += \
-    android.hardware.biometrics.fingerprint@2.1
+    android.hardware.biometrics.fingerprint@2.1 \
+	android.hardware.biometrics.fingerprint@2.1-service.m1852
 
 # GPS
 PRODUCT_PACKAGES += \
