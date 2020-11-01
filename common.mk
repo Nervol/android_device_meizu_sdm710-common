a015f2743619fe94ad2116dd36ec6aacc0068f30
blackrow67 <dogukanbey42@gmail.com>
2020-11-01 09:23:49 +0000

    Revert original common.mk

--- common.mk
+++ common.mk
@@ -125,8 +125,7 @@ PRODUCT_PACKAGES += \
 
 # Fingerprint
 PRODUCT_PACKAGES += \
-    android.hardware.biometrics.fingerprint@2.1 \
-	android.hardware.biometrics.fingerprint@2.1-service.m1852
+    android.hardware.biometrics.fingerprint@2.1
 
 # GPS
 PRODUCT_PACKAGES += \
