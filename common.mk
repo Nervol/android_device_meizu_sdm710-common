3f0ca0f4a878feeea26655c8ec2504f4b25f2e27
blackrow67 <dogukanbey42@gmail.com>
2020-12-12 13:57:35 +0000

    try to fix gapps and init: CleanUp init *.rc scripts

--- common.mk
+++ common.mk
@@ -223,6 +223,7 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/privapp-permissions-qti.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/privapp-permissions-qti.xml \
     $(LOCAL_PATH)/configs/qti_whitelist.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/sysconfig/qti_whitelist.xml
+    $(LOCAL_PATH)/configs/privapp-permission/privapp-permissions-google-extra.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/privapp-permissions-google-extra.xml
 
 # Qualcomm vndfwk detect
 PRODUCT_PACKAGES += \
